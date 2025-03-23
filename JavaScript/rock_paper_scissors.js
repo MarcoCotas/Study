dc67687b6284ef1de06ed1f898c3d106568bbfc7
MarcoCotas <marco.cotas06@gmail.com>
2025-03-23 17:37:03 +0000

    Create function to get random CPU choice

--- JavaScript/rock_paper_scissors.js
+++ JavaScript/rock_paper_scissors.js
@@ -3,4 +3,17 @@
 //Create Function with rules of game
 // Play game and register results
 
-const userChoice = prompt("Rock, Paper or Scissors?")
+const userChoice = prompt("Rock, Paper or Scissors?");
+const cpuChoice = getCpuChoice(3)
+console.log (userChoice)
+console.log (cpuChoice)
+
+function getCpuChoice(num){
+    let choice =  Math.floor(Math.random() *num)
+    if (choice === 0){
+        return "Paper"
+    } else if (choice === 1){
+        return "Rock"
+    }else if (choice === 2){
+        return "Scissors"}
+}
