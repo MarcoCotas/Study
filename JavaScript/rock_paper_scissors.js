6f658d9640cb524d586e6bbf76d8273ee6f7e89a
MarcoCotas <marco.cotas06@gmail.com>
2025-03-23 17:29:29 +0000

    Start coding RPS by my own

---
+++ JavaScript/rock_paper_scissors.js
@@ -0,0 +1,6 @@
+//Ask user for a valid choice
+//Generate Random PC Choice
+//Create Function with rules of game
+// Play game and register results
+
+const userChoice = prompt("Rock, Paper or Scissors?")
