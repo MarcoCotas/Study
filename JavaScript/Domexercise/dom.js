f9d6c75c35a7f8cb1c62a7c724e2da7fdc33cc90
Marco Cotas <marco.cotas06@gmail.com>
2025-03-25 09:35:06 +0000

    Finish the Dom Exercise

--- JavaScript/Domexercise/dom.js
+++ JavaScript/Domexercise/dom.js
@@ -1,13 +1,24 @@
 const container = document.querySelector("#container");
 const redParagraph = document.createElement("p");
 const heading3 = document.createElement("h3");
+const mainDiv = document.createElement("div");
+const heading1 = document.createElement("h1");
+const paragraph = document.createElement("p");
 
 redParagraph.classList.add("red-paragraph");
 redParagraph.textContent = "This is a red-paragraph";
 heading3.textContent = "I'm a blue h3!";
-
-container.appendChild(redParagraph);
-container.appendChild(heading3);
+heading1.textContent = "Im in a div";
+paragraph.textContent = "ME TOO!";
 
 redParagraph.style.color = "red";
 heading3.style.color = " blue";
+mainDiv.style.border = "2px solid black";
+heading1.style.color = "black";
+paragraph.style.color = "black";
+
+container.appendChild(redParagraph);
+container.appendChild(heading3);
+mainDiv.appendChild(heading1);
+mainDiv.appendChild(paragraph);
+container.appendChild(mainDiv);
